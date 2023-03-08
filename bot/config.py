660456f5c12ac36f480f89b5214acb6431788560
Your Name <you@example.com>
2023-03-08 23:15:14 +0200

    updated config, now using only yml files

--- bot/config.py
+++ bot/config.py
@@ -5,8 +5,12 @@ from pathlib import Path
 config_dir = Path(__file__).parent.parent.resolve() / "config"
 
 # load yaml config
-with open(config_dir / "config.yml", 'r') as f:
+with (config_dir / "config.yml").open() as f:
     config_yaml = yaml.safe_load(f)
+
+with (config_dir / "secrets.yml").open() as f:
+    secrets_yaml = yaml.safe_load(f)
+config_yaml.update(secrets_yaml)
 
 # load .env config
 config_env = dotenv.dotenv_values(config_dir / "config.env")
@@ -17,7 +21,7 @@ openai_api_key = config_yaml["openai_api_key"]
 use_chatgpt_api = config_yaml.get("use_chatgpt_api", True)
 allowed_telegram_usernames = config_yaml["allowed_telegram_usernames"]
 new_dialog_timeout = config_yaml["new_dialog_timeout"]
-mongodb_uri = f"mongodb://mongo:{config_env['MONGODB_PORT']}"
+mongodb_uri = config_yaml["MONGODB_URL"]
 
 # prices
 chatgpt_price_per_1000_tokens = config_yaml.get("chatgpt_price_per_1000_tokens", 0.002)
